f4375399123c22b943a5534a129683cfd22674ca
Abdelrahman Ahmed Hafez <Abdelrahmanelshabiny@gmail.com>
2025-09-30 00:29:10 +0300

    Added an inital Robot API

---
+++ controllers/CustomBot.py
@@ -0,0 +1,119 @@
+from controller import Robot
+import math
+import struct
+
+# Constants
+MAX_SPEED = 6.28
+
+# Message type constants (match referee)
+MSG_DEPOSIT = 1
+MSG_COLLECT = 2
+MSG_TIMER = 3
+
+# Example: map colors/types to integers
+# (this must match your utils.config SCORING_RULES)
+COLLECTABLE_TYPES = {
+    "red": 0,
+    "blue": 1,
+    "black": 2
+}
+
+class CustomBot:
+    def __init__(self, robot, ROBOT_ID):
+        self.robot = robot
+        self.robot_id = ROBOT_ID
+        self.timestep = int(robot.getBasicTimeStep())
+
+
+        self.emitter = self.robot.getDevice("emitter")
+        self.receiver = self.robot.getDevice("receiver")
+        self.receiver.enable(self.timestep)
+
+        self.gps = self.robot.getDevice("gps")
+        self.gps.enable(self.timestep)
+
+        self.compass = self.robot.getDevice("compass")
+        self.compass.enable(self.timestep)
+
+        self.distance_left = self.robot.getDevice("distancesensor left")
+        self.distance_left.enable(self.timestep)
+        self.distance_right = self.robot.getDevice("distancesensor right")
+        self.distance_right.enable(self.timestep)
+        self.distance_front = self.robot.getDevice("distancesensor front")
+        self.distance_front.enable(self.timestep)
+        self.distance_back = self.robot.getDevice("distancesensor back")
+        self.distance_back.enable(self.timestep)
+
+        self.status_led = self.robot.getDevice("status led")
+        self.led_state = 0
+
+        self.left_motor = self.robot.getDevice("left wheel motor")
+        self.right_motor = self.robot.getDevice("right wheel motor")
+
+        self.left_motor.setPosition(float("+inf"))
+        self.right_motor.setPosition(float("+inf"))
+
+        self.left_motor.setVelocity(0.0)
+        self.right_motor.setVelocity(0.0)
+
+
+    def set_speed(self, left, right):
+        """Set wheel speeds."""
+        self.left_motor.setVelocity(left)
+        self.right_motor.setVelocity(right)
+
+    def get_position(self):
+        """Return GPS (x, y)."""
+        pos = self.gps.getValues()
+        return [pos[0], pos[1]]
+
+    def get_orientation(self):
+        """Return compass heading in radians."""
+        north = self.compass.getValues()
+        rad = math.atan2(north[0], north[1])
+        return rad
+
+    def read_distances(self):
+        """Return dict of all distance sensor readings."""
+        return {
+            "left": self.distance_left.getValue(),
+            "right": self.distance_right.getValue(),
+            "front": self.distance_front.getValue(),
+            "back": self.distance_back.getValue()
+        }
+
+    def send_message(self, msg_type, data):
+        """Send raw message to supervisor/referee."""
+        message = struct.pack('iii', self.robot_id, msg_type, data)
+        self.emitter.send(message)
+
+    def receive_message(self):
+        """Check for a message from supervisor/referee."""
+        if self.receiver.getQueueLength() > 0:
+            msg = self.receiver.getData()
+            self.receiver.nextPacket()
+            return msg
+        return None
+
+    def toggle_led(self):
+        self.led_state = not self.led_state
+        self.status_led.set(self.led_state)
+
+    def get_time(self):
+        '''Send a time request to supervisor and return the current time'''
+        self.send_message(MSG_TIMER, 0)
+        while True:
+            time = self.receive_message()
+            if time:
+                return time
+
+    def wait(self, seconds):
+        counter = 0
+        while counter < (seconds / self.timestep):
+            self.robot.step(self.timestep)
+            counter += 1
+
+    
+    
+            
+
--- controllers/Team_blue/Team_blue.py
+++ controllers/Team_blue/Team_blue.py
@@ -1,104 +1,51 @@
 from controller import Robot
-import struct
+from CustomBot import *
 
 # Create the Robot instance
 robot = Robot()
-
-# Get the simulation timestep
-timestep = int(robot.getBasicTimeStep())
-
-# Initialize motors
-left_motor = robot.getDevice('left wheel motor')
-right_motor = robot.getDevice('right wheel motor')
-left_motor.setPosition(float('inf'))  # velocity control mode
-right_motor.setPosition(float('inf'))
-left_motor.setVelocity(0.0)
-right_motor.setVelocity(0.0)
-
-# Initialize proximity sensors
-ps = []
-ps_names = ['ps0','ps1','ps2','ps3','ps4','ps5','ps6','ps7']
-for name in ps_names:
-    sensor = robot.getDevice(name)
-    sensor.enable(timestep)
-    ps.append(sensor)
-
-# Initialize emitter
-emitter = robot.getDevice('emitter')
-if emitter is None:
-    print("Warning: No emitter device found")
-
-# Robot identification
 ROBOT_ID = 0  # 0 = blue, 1 = red
-MAX_SPEED = 6.28
 
-# Message type constants (match referee)
-MSG_DEPOSIT = 1
-MSG_COLLECT = 2
+# Wrap with our API
+myRobot = CustomBot(robot, ROBOT_ID)
 
-# Example: map colors/types to integers
-# (this must match your utils.config SCORING_RULES)
-COLLECTABLE_TYPES = {
-    "red": 0,
-    "blue": 1,
-    "black": 2
-}
-
-collect_types = list(COLLECTABLE_TYPES.keys())
+collect_types = ["red", "blue", "black"]
 collect_index = 0  # keeps track of which type to send
 
-
-def send_collect(robot_id, claimed_type):
-    """Send a collect request to the referee"""
-    if emitter:
-        message = struct.pack('iii', robot_id, MSG_COLLECT, claimed_type)
-        emitter.send(message)
-        print(f"Robot {robot_id} sent COLLECT request for type {claimed_type}")
-
-def send_deposit(robot_id):
-    """Send a deposit request to the referee"""
-    if emitter:
-        message = struct.pack('iii', robot_id, MSG_DEPOSIT, 0)
-        emitter.send(message)
-        print(f"Robot {robot_id} sent DEPOSIT request")
-
-# Example simple movement + fake collection
+# Simple test loop
 step_count = 0
 collection_cooldown = 0
 
-while robot.step(timestep) != -1:
+while robot.step(myRobot.timestep) != -1:
     step_count += 1
 
-    # Read sensors
-    ps_values = [sensor.getValue() for sensor in ps]
-    right_obstacle = ps_values[0] > 80.0 or ps_values[1] > 80.0 or ps_values[2] > 80.0
-    left_obstacle  = ps_values[5] > 80.0 or ps_values[6] > 80.0 or ps_values[7] > 80.0
+    # --- Simple obstacle avoidance using distance sensors ---
+    distances = myRobot.read_distances()
+    left_obstacle = distances["left"] > 80.0
+    right_obstacle = distances["right"] > 80.0
 
-    # Simple obstacle avoidance
     left_speed, right_speed = MAX_SPEED, MAX_SPEED
     if left_obstacle:
-        left_speed, right_speed = MAX_SPEED, -MAX_SPEED/2
+        left_speed, right_speed = MAX_SPEED, -MAX_SPEED / 2
     elif right_obstacle:
-        left_speed, right_speed = -MAX_SPEED/2, MAX_SPEED
+        left_speed, right_speed = -MAX_SPEED / 2, MAX_SPEED
 
-    left_motor.setVelocity(left_speed)
-    right_motor.setVelocity(right_speed)
+    myRobot.set_speed(left_speed, right_speed)
 
-    # Cooldown
+    # --- Collect cooldown ---
     if collection_cooldown > 0:
         collection_cooldown -= 1
 
-    # 🔹 Fake logic for testing:
-    # Every 200 steps, attempt to collect a "green" box
+    # 🔹 Fake logic for testing
+    # Every 200 steps, send COLLECT message
     if step_count % 200 == 0 and collection_cooldown == 0:
         collect_type = collect_types[collect_index]
-        send_collect(ROBOT_ID, COLLECTABLE_TYPES[collect_type])
+        myRobot.send_message(myRobot.MSG_COLLECT, myRobot.COLLECTABLE_TYPES[collect_type])
+        print(f"Robot {ROBOT_ID} sent COLLECT request for type {collect_type}")
 
-        # Move to next type (wrap around at the end)
         collect_index = (collect_index + 1) % len(collect_types)
-
         collection_cooldown = 50
 
-    # Every 500 steps, attempt a deposit
+    # Every 500 steps, send DEPOSIT message
     if step_count % 500 == 0:
-        send_deposit(ROBOT_ID)
+        myRobot.send_message(myRobot.MSG_DEPOSIT, 0)
+        print(f"Robot {ROBOT_ID} sent DEPOSIT request")
